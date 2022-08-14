c208a5be8aeaea497e286b4c39623821c997f8d0
Matthew Tole <matthewtole@gmail.com>
2022-08-13 18:30:34 -0700

    Add some Arduino code

---
+++ arduino/keypad/keypad.ino
@@ -0,0 +1,40 @@
+int outputPins[] = { 2, 3,  4,  5 };
+int inputPins[]  = { 8, 9, 10, 11 };
+
+char buttons[ 4 ][ 4 ] = {
+  { '?', '?', '?', '?' },
+  { '?', '?', '?', '?' },
+  { '?', '?', '?', '?' },
+  { '?', '?', '?', '?' }
+};
+
+void setup() {
+  for (int i = 0; i < 4; i += 1 ) {
+    pinMode(outputPins[i], INPUT_PULLUP);
+  }
+  for (int i = 0; i < 4; i += 1 ) {
+    pinMode(inputPins[i], INPUT_PULLUP);
+  }
+
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  for (int out = 0; out < 4; out += 1 ) {
+    pinMode(outputPins[out], OUTPUT);
+    digitalWrite(outputPins[out], LOW);
+    for (int in = 0; in < 4; in += 1 ) {
+      if (digitalRead(inputPins[in]) == LOW) {
+        Serial.print("Match: ");
+        Serial.print(buttons[in][out]);
+        Serial.print("(");
+        Serial.print(in);
+        Serial.print(", ");
+        Serial.print(in);
+        Serial.print(")");
+      }
+    }
+    pinMode(outputPins[out], INPUT_PULLUP);
+  }
+}
