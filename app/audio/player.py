aa079e9b0edc63e5218f0dab987de55413ef3848
Matthew Tole <matthewtole@gmail.com>
2022-08-26 13:39:35 -0700

    Switch to subprocess based audio player

--- app/audio/player.py
+++ app/audio/player.py
@@ -1,50 +1,21 @@
 import logging
-from typing import Mapping, Optional
-import pyaudio
-import wave
+import sys
+import subprocess
 
 
 class AudioPlayer:
     def __init__(self):
         super().__init__()
-        self.audio = pyaudio.PyAudio()
         self.is_playing = False
         self.log = logging.getLogger("Audio.Player")
-        self.stream = None
-
-    def callback(
-        self,
-        in_data: Optional[bytes],
-        frame_count: int,
-        time_info: Mapping[str, float],
-        status: int,
-    ):
-        data = self.wave_file.readframes(frame_count)
-        return (data, pyaudio.paContinue)
 
     def play(self, filename: str) -> None:
         self.log.log(logging.INFO, "Playing %s" % filename)
-        self.wave_file = wave.open(filename, "rb")
-        self.stream = self.audio.open(
-            format=self.audio.get_format_from_width(self.wave_file.getsampwidth()),
-            channels=self.wave_file.getnchannels(),
-            rate=self.wave_file.getframerate(),
-            frames_per_buffer=512,
-            output=True,
-            stream_callback=self.callback,
-        )
-
-        self.stream.start_stream()
         self.is_playing = True
+        self._p = subprocess.Popen(["afplay" if sys.platform == "darwin" else "aplay", filename])
 
     def stop(self) -> None:
         self.log.log(logging.INFO, "Audio playback stopped")
-        if self.stream is not None:
-            self.stream.stop_stream()
 
     def tick(self) -> None:
-        if self.is_playing and self.stream is not None and not self.stream.is_active():
-            self.stream.stop_stream()
-            self.stream.close()
-            self.wave_file.close()
-            self.is_playing = False
+        self.is_playing = (self._p.poll() is None)
